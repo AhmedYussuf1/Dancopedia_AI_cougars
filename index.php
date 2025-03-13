a7f8662132e793af9c38361dec7237d81ec498d6
BuildTools <unconfigured@null.spigotmc.org>
2025-03-12 20:26:28 -0500

    revert bootstrap script change

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ $result = $conn->query($sql);
     </div>
 
     <!-- Bootstrap JS and dependencies -->
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <script>
--- interactive_map.php
+++ interactive_map.php
@@ -190,6 +190,6 @@ async function getCoordinates(city) {
             
     </script>
 </body>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>

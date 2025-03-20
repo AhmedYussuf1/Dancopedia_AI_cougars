b3e3486ce1cd97bd9d97f530c2e6ee682e18a8dc
Ahmed <ahmed.yussuf@my.metrostate.edu>
2025-03-19 20:06:42 -0500

    Update folk_dances.php

--- folk_dances.php
+++ folk_dances.php
@@ -7,7 +7,7 @@ include('db_connection.php');
 include('getTheme.php');
 require_once('utility_functions/display_result.php');
  // Query to get  from dance table with genre name Classical
- $sql = "SELECT * FROM `dances` WHERE  genre='Hip-hop'  ";  // Ensure this matches your table and column names
+ $sql = "SELECT * FROM `dances` WHERE  genre='folk'  ";  // Ensure this matches your table and column names
  $result = $conn->query($sql);
  
 

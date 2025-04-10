4bab360b4d8f6087824602ece35767fd50235c20
BuildTools <unconfigured@null.spigotmc.org>
2025-04-09 20:26:06 -0500

    Fix dropdown for user settings

--- dance_view.php
+++ dance_view.php
@@ -165,8 +165,9 @@ function getGenre($conn){
     </div>
 </div>
 
-
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- Bootstrap JS and dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 <?php
 include('footer.php');

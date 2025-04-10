acaf694ae09cabd3b9095b48009e32caed1e52c0
BuildTools <unconfigured@null.spigotmc.org>
2025-04-09 22:07:08 -0500

    Added output buffering to reduce conflicts with navbar

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();
 include('db_connection.php');  // Include your database connection
 
 // Handle POST request for registration
@@ -121,6 +121,7 @@ function insertUserSettings($user_id, $conn) {
 
 // Close the connection
 $conn->close();
+ob_end_flush();
 ?>
 <!DOCTYPE html>
 <html lang="en">
